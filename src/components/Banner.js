9c269d27910bbbd340375bdc921dfcefb07939d1
HarrisonStrand <harrisonstrand@gmail.com>
2021-02-12 14:52:49 -0800

    add banner to project

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,10 @@
 import React from "react";
+import beerbanner from './../img/beerbanner.jpg';
 
 function Banner(){
   return (
     <>
+      <img src={beerbanner} alt="An image of a logo" />
 		</>
   );
 }
