651d29850be5747ee614703304436e02fb281b51
HarrisonStrand <harrisonstrand@gmail.com>
2021-02-12 09:37:55 -0800

    add Keg component with proptypes

---
+++ src/components/Keg.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Keg(props){
+	return (
+		<>
+			<div onClick = {() => props.whenKegClicked(props.id)}>
+				<p>{props.name}</p>
+			</div>
+		</>
+	);
+}
+
+Keg.propTypes = {
+	name: PropTypes.string,
+	brand: PropTypes.string,
+	price: PropTypes.number,
+	ABV: PropTypes.number,
+	pints: PropTypes.number,
+	id: PropTypes.string,
+	whenKegClicked: PropTypes.func
+}
