8bd219f12dcced549d4fceb9407a4304331ae0d3
HarrisonStrand <harrisonstrand@gmail.com>
2021-02-19 09:45:50 -0800

    add good pass for add keg to list for keglistreducer

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,19 @@
 export default (state = {}, action) => {
-	return state;
-	}
+	const { name, brand, price, ABV, pints, stock, id } = action;
+	switch (action.type) {
+		case 'ADD_KEG':
+			return Object.assign({}, state, {
+				[id] : {
+					name: name,
+					brand: brand,
+					price: price,
+					ABV: ABV,
+					pints: pints,
+					stock: stock,
+					id: id
+				}
+			});
+		default:
+				return state;
+		}
+	};
