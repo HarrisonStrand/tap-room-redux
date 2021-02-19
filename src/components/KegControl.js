f89a27a9cc13bda86bc719ab061efe5cd4f3930f
HarrisonStrand <harrisonstrand@gmail.com>
2021-02-19 10:25:02 -0800

    add redux functionality to KegControl with mapStateToProps and propTypes

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -4,41 +4,54 @@ import KegList from './KegList';
 import KegDetail from './KegDetail';
 import EditKegForm from './EditKegForm';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Keg from './Keg';
 
 class KegControl extends React.Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			// formVisibleOnPage: false,
 			selectedKeg: null,
 			editing: false
 		};
 	}
 
 	handleAddingNewKegToList = (newKeg) => {
-		const newMasterKegList = this.state.masterKegList
-			.concat(newKeg);
-		this.setState({ 
-			masterKegList: newMasterKegList, 
-			formVisibleOnPage: false });
+		const { dispatch } = this.props;
+		const { name, brand, price, ABV, pints, stock, id } = newItem;
+		const action = {
+			type: 'ADD_KEG',
+			name: name,
+			brand: brand,
+			price: price,
+			ABV: ABV,
+			pints: pints,
+			stock: stock,
+			id: id
+		}
+		dispatch (action);
+		const action2 = {
+			type: 'TOGGLE_FORM'
+		}
+		dispatch(action2);
 	}
 
 	handleChangingSelectedKeg = (id) => {
-		const selectedKeg = this.state.masterKegList
-			.filter(keg => keg.id === id)[0];
+		const selectedKeg = this.props.masterKegList[id];
 		this.setState({
 			selectedKeg: selectedKeg
 		});
 	}
 
 	handleDeletingKeg = (id) => {
-		const newMasterKegList = this.state.masterKegList
-			.filter(keg => keg.id !== id);
-		this.setState({
-			masterKegList: newMasterKegList,
-			selectedKeg: null
-		});
+		const { dispatch } = this.props;
+		const action = {
+			type: 'DELETE_KEG',
+			id: id
+		}
+		dispatch(action);
+		this.setState({selectedKeg: null});
 	}
 
 	handleEditClick = () => {
@@ -48,11 +61,20 @@ class KegControl extends React.Component {
 	}
 
 	handleEditingKegInList = (kegToEdit) => {
-		const editedMasterKegList = this.state.masterKegList
-			.filter(keg => keg.id !== this.state.selectedKeg.id)
-			.concat(kegToEdit);
+		const { dispatch } = this.props;
+		const { name, brand, price, ABV, pints, stock, id } = kegToEdit;
+		const action = {
+			type: 'ADD_KEG',
+			name: name,
+			brand: brand,
+			price: price,
+			ABV: ABV,
+			pints: pints,
+			stock: stock,
+			id: id
+		}
+		dispatch(action);
 		this.setState({
-			masterKegList: editedMasterKegList,
 			editing: false,
 			selectedKeg: null
 		});
@@ -62,45 +84,37 @@ class KegControl extends React.Component {
 		const selectedKeg = this.state.selectedKeg;
 		if (selectedKeg.pints > 11) {
 			const newQuantity = Object.assign({}, selectedKeg, {pints: selectedKeg.pints - 1});
-			const newKegList = this.state.masterKegList
-				.filter(keg => keg.id !== this.state.selectedKeg.id)
-				.concat(newQuantity);
-				this.setState({
-					masterKegList: newKegList,
-					selectedKeg: newQuantity
-				});
+			this.handleEditingKegInList(newQuantity);
+			this.setState({
+				selectedKeg: newQuantity
+			});
 		} else if (selectedKeg.pints > 0 && selectedKeg.pints <= 11) {
 			const newQuantity = Object.assign({}, selectedKeg, {pints: selectedKeg.pints - 1}, {stock: selectedKeg.stock = "Almost Empty!"});
-			const newKegList = this.state.masterKegList
-				.filter(keg => keg.id !== this.state.selectedKeg.id)
-				.concat(newQuantity);
-				this.setState({
-					masterKegList: newKegList,
-					selectedKeg: newQuantity
-				});
+			this.handleEditingKegInList(newQuantity);
+			this.setState({
+				selectedKeg: newQuantity
+			});
 		} else {
 			const newQuantity = Object.assign({}, selectedKeg, {stock: selectedKeg.stock = "Out Of Stock!"});
-			const newKegList = this.state.masterKegList
-				.filter(keg => keg.id !== this.state.selectedKeg.id)
-				.concat(newQuantity);
-				this.setState({
-					masterKegList: newKegList,
-					selectedKeg: newQuantity
-				});
+			this.handleEditingKegInList(newQuantity);
+			this.setState({
+				selectedKeg: newQuantity
+			});
 		}
 	}
 
 	handleClick = () => {
 		if (this.state.selectedKeg != null) {
 			this.setState({
-				formVisibleOnPage: false,
 				selectedKeg: null,
 				editing: false
 			});
 		} else {
-			this.setState(prevState => ({
-				formVisibleOnPage: !prevState.formVisibleOnPage
-			}));
+			const { dispatch } = this.props;
+			const action = {
+				type: 'TOGGLE_FORM'
+			}
+			dispatch(action);
 		}
 	}
 
@@ -122,7 +136,7 @@ class KegControl extends React.Component {
 					onClickingPurchase = {this.handlePurchasingKegPintInList}
 					/>
 					buttonText = "Keg List";
-		} else if (this.state.formVisibleOnPage) {
+		} else if (this.props.formVisibleOnPage) {
 			currentlyVisibleState =
 				<NewKegForm
 					onNewKegCreation = {this.handleAddingNewKegToList}
@@ -132,7 +146,7 @@ class KegControl extends React.Component {
 		} else {
 			currentlyVisibleState = 
 				<KegList
-					kegList = {this.state.masterKegList}
+					kegList = {this.props.masterKegList}
 					onKegSelection = {this.handleChangingSelectedKeg}
 					/>
 					buttonText = "Create New Keg";
@@ -145,5 +159,19 @@ class KegControl extends React.Component {
 		)
 	}
 }
+
+KegControl.propTypes = {
+	masterKegList: PropTypes.object,
+	formVisibleOnPage: PropTypes.bool
+}
+
+const mapStateToProps = state => {
+	return {
+		masterKegList: state.masterKegList,
+		formVisibleOnPage: state.formVisibleOnPage
+	}
+}
+
+KegControl = connect(mapStateToProps)(KegControl);
 
 export default KegControl;
--- src/index.js
+++ src/index.js
@@ -4,15 +4,21 @@ import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
+
+const store = createStore(rootReducer);
+
+store.subscribe(() =>
+  console.log(store.getState()) //delete after finalizing project
+);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
