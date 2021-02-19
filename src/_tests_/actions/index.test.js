40a1e4a1d2dda64f2a85a63d649dc7c2a4cfae36
HarrisonStrand <harrisonstrand@gmail.com>
2021-02-19 11:05:24 -0800

    add fail for TOGGLE_FORM action creator

--- src/_tests_/actions/index.test.js
+++ src/_tests_/actions/index.test.js
@@ -7,4 +7,11 @@ describe('tap room actions', () => {
       id: 1
     });
   });
+
+  it('toggleForm should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
+  });
+  
 });
