6e40d2e6908b5106d1c599e86972c13408508816
HarrisonStrand <harrisonstrand@gmail.com>
2021-02-19 09:53:44 -0800

    add good fail for TOGGLE_FORM of form-visible-reducer

--- src/_tests_/reducers/form-visible-reducer.test.js
+++ src/_tests_/reducers/form-visible-reducer.test.js
@@ -6,4 +6,8 @@ describe('formVisibleReducer', () => {
 		expect(formVisibleReducer(false, { type: null })).toEqual(false);
 	});
 
+	test('Should toggle form visibility state to true', () => {
+		expect(formVisibleReducer(false, { type: 'TOGGLE_FORM' })).toEqual(true);
+	});
+
 });
