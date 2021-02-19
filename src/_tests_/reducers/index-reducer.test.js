7416b71733dddf6bdb32d44a2b0498e46bbc609c
HarrisonStrand <harrisonstrand@gmail.com>
2021-02-19 10:32:13 -0800

    add fail for checking initial state against rootReducer

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -8,4 +8,13 @@ describe('rootReducer', () => {
 			formVisibleOnPage: false
 		});
 	});
+
+	test('Check that initial state of kegListReducer matches rootReducer', () => {
+		expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, { type: null }));
+	});
+
+	test('Check that initial state of formVisibleReducer matches rootReducer', () => {
+		expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+	});
+	
 });
