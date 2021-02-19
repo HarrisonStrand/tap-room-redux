d59613019a8d32fb0dea525d60fe3154cf1cd07e
HarrisonStrand <harrisonstrand@gmail.com>
2021-02-19 10:31:13 -0800

    add pass for default state of index combine reducers

--- src/reducers/index-reducer.js
+++ src/reducers/index-reducer.js
@@ -3,7 +3,8 @@ import kegListReducer from './keg-list-reducer';
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
-
+	formVisibleOnPage: formVisibleReducer,
+	masterKegList: kegListReducer
 });
 
 export default rootReducer;
