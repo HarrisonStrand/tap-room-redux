3faa3ea0a20ea7c9fee5fd17f3853d3f3c2718c5
HarrisonStrand <harrisonstrand@gmail.com>
2021-02-19 11:31:18 -0800

    add action constants to action creators and action creator tests

--- src/_tests_/actions/index.test.js
+++ src/_tests_/actions/index.test.js
@@ -1,16 +1,17 @@
+import { DELETE_KEG, TOGGLE_FORM } from '../../actions/ActionTypes';
 import * as actions from './../../actions';
 
 describe('tap room actions', () => {
   it('deleteKeg should create DELETE_KEG action', () => {
     expect(actions.deleteKeg(1)).toEqual({
-      type: 'DELETE_KEG',
+      type: DELETE_KEG,
       id: 1
     });
   });
 
   it('toggleForm should create TOGGLE_FORM action', () => {
     expect(actions.toggleForm()).toEqual({
-      type: 'TOGGLE_FORM'
+      type: TOGGLE_FORM
     });
   });
 
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,16 @@
+import * as c from './ActionTypes';
+
 export const deleteKeg = id => ({
-  type: 'DELETE_KEG',
+  type: c.DELETE_KEG,
   id
 });
 export const toggleForm = () => ({
-  type: 'TOGGLE_FORM'
+  type: c.TOGGLE_FORM
 });
 export const addKeg = (keg) => {
   const { name, brand, price, ABV, pints, stock, id } = keg;
   return {
-    type: 'ADD_KEG',
+    type: c.ADD_KEG,
     name,
     brand,
     price,
